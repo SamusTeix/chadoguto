b0fd447ed196ad7c36b5e8be9b2357bdd61976aa
SamusTeix <samuel.itsl@live.com>
2020-09-12 17:22:59 -0300

    [ chadoguto ] - Ajuste pagina login adm

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,11 +1,21 @@
 @extends('master')
 
 @section('content')
-	
-    <form method="POST" action="/admin/login">
-    	@csrf
-    	Login: <input type="text" name="login">
-    	Senha: <input type="password" name="senha">
-    	<input type="submit" value="Entrar">
-    </form>
+	<form method="POST" action="/admin/login">
+		<div class="row">
+			<div class="col-12">
+				<div class="form-group">
+					<label for="nome">Login:</label>
+					<input type="text" class="form-control" id="login" name="login">
+				</div>
+				<div class="form-group">
+					<label for="nome">Senha:</label>
+					<input type="password" class="form-control" id="senha" name="senha">
+				</div>		
+			</div>
+			<div class="col-12 text-right">
+				<input type="submit" value="Entrar">		
+			</div>
+		</div>
+	</form>
 @endsection
